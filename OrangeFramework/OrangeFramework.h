5b27189de9d6dc4f8b0c6bbf41d7c3f2d2526506
raghu <raghu@Mac-mini-2.local>
2017-07-24 15:25:05 +0530

    initial commit

---
+++ OrangeFramework/OrangeFramework.h
@@ -0,0 +1,19 @@
+//
+//  OrangeFramework.h
+//  OrangeFramework
+//
+//  Created by raghu on 7/24/17.
+//  Copyright © 2017 raghu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OrangeFramework.
+FOUNDATION_EXPORT double OrangeFrameworkVersionNumber;
+
+//! Project version string for OrangeFramework.
+FOUNDATION_EXPORT const unsigned char OrangeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OrangeFramework/PublicHeader.h>
+
+
